b8710b0c3c204e790290785cb8cde6917bb388f7
Veljko Simic <veljko.simic98@gmail.com>
2016-04-10 14:57:00 +0200

    filtering tiket by state

--- api/v1/views.py
+++ api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route
 from v1 import serializers
+from .models import Tiket
 
 
 class SpisakViewSet(viewsets.ModelViewSet):
@@ -10,9 +11,33 @@ class SpisakViewSet(viewsets.ModelViewSet):
     queryset = serializer_class.Meta.model.objects.all()
 
     @detail_route(methods=['get'])
-    def tikets(self, request, pk=None):
+    def all(self, request, pk=None):
         spisak = self.get_object()
         tikets = spisak.tiket_set.all()
+        serializer = serializers.TiketSerializer(tikets, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    @detail_route(methods=['get'])
+    def isporuceno(self, request, pk=None):
+        spisak = self.get_object()
+        tikets = Tiket.objects.all()
+        tikets = tikets.filter(spisak=spisak.id, stanje="isporuceno")
+        serializer = serializers.TiketSerializer(tikets, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    @detail_route(methods=['get'])
+    def ne_isporuceno(self, request, pk=None):
+        spisak = self.get_object()
+        tikets = Tiket.objects.all()
+        tikets = tikets.filter(spisak=spisak.id, stanje="ne_isporuceno")
+        serializer = serializers.TiketSerializer(tikets, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    @detail_route(methods=['get'])
+    def obrisano(self, request, pk=None):
+        spisak = self.get_object()
+        tikets = Tiket.objects.all()
+        tikets = tikets.filter(spisak=spisak.id, stanje="obrisano")
         serializer = serializers.TiketSerializer(tikets, context={'request': request}, many=True)
         return Response(serializer.data)
 
